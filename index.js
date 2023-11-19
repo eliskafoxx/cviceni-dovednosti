b3d5f99bfbf6a045ea957765e9e9b27040c609bd
„Eliška“ <eliska.foxx@gmail.com>
2023-11-19 14:18:00 +0100

    ukol cviceni-done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('funguju!');
+function updateSkill(skillId, vstup) {
+    const skillElement = document.getElementById(skillId)
+    const skillProgress = skillElement.querySelector('.skill__progress')
+    const skillValue = skillElement.querySelector('.skill__value')
+ 
+    skillProgress.style.width = vstup + '%'
+    skillValue.textContent = vstup + ' / 100'
+  }
+  
+const dovednostHTML = Number(prompt("Zadej svou úroveň znalosti HTML od 1 do 100:"))
+const dovednostCSS = Number (prompt("Zadej svou úroveň znalosti CSS od 1 do 100:"))
+const dovednostJS = Number (prompt("Zadej svou úroveň znalosti JavaScript od 1 do 100:"))
+ 
+updateSkill('skill1', dovednostHTML)
+updateSkill('skill2', dovednostCSS)
+updateSkill('skill3', dovednostJS)
